d5f27b6c6d980ec377c08c4c47a36153723f7e41
Fernandamluz <78964089+Fernandamluz@users.noreply.github.com>
2022-03-15 09:48:24 -0300

    Update codigo.ino

--- Aula 02/codigo.ino
+++ Aula 02/codigo.ino
@@ -1,5 +1,6 @@
 const int botao1 = 2;
 const int botao2 = 3;
+const int led = 4;
 
 int var1;
 int var2;
@@ -8,6 +9,7 @@ int var2;
  { 
     pinMode(botao1, INPUT);
   	pinMode(botao2, INPUT);
+    pinMode(led, OUTPUT);  
    	Serial.begin(9600);
  }
  
@@ -17,9 +19,11 @@ int var2;
    	var2 = digitalRead(botao2);
    	
    if(var1){
-     Serial.println("botao 1 apertado");
+    Serial.println("botao 1 apertado");
+    digitalWrite(led, HIGH);
    }
    if(!var2){
-     Serial.println("botao 2 apertado");
+    Serial.println("botao 2 apertado");
+    digitalWrite(led, LOW);
    }
  }
